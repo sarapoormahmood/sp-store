c229d4cd0b84a11f77fbffc7879469850d686667
Sara <poormahmoodsara@gmail.com>
2026-08-25 23:54:17 +0330

    fix blob image display

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.public.blob.vercel-storage.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
